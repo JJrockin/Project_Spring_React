d6cd54500a75ebdd09287fbf3d03960d8d4534fd
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2022-11-22 11:28:45 -0300

    other components

---
+++ Projetos/dsmeta/frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import image from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={image} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+
+                    <a href="https://www.instagram.com/jeronimomoraesf"> @jeronimomoraesf </a>
+
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
